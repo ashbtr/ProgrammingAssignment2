b94edc9a39fc6e3d4d8319a43d24e1259cacaba7
Ashwin Bhirud <ashbtr@gmail.com>
2017-12-07 17:43:27 -0500

    Added my code for solution to Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My first function is a nested function that uses lexical scoping
+## to contain 'get' and 'set' functions that allow caching of 
+## matrix inversions. The second function inverts the matrix if a 
+## cached value is not present
 
-## Write a short comment describing this function
+## The makeCacheMatrix function contains two objects and four
+## nested functions. These nested functions allow the 
+## input matrix and its inversion to be cached in the 
+## parent environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function()m
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function checks for a cached inversion of the input
+## and if there is no cached value, it will compute one
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinv(m)
+    m
 }
